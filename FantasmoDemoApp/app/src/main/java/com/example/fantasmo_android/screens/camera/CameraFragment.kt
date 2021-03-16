f00e38144a696883f4449858e89d12bcbfcfccbc
José Silva <jose.silva@fsstudio.com>
2021-03-16 18:28:20 +0000

    Added Location tracker

--- FantasmoDemoApp/app/src/main/java/com/example/fantasmo_android/screens/camera/CameraFragment.kt
+++ FantasmoDemoApp/app/src/main/java/com/example/fantasmo_android/screens/camera/CameraFragment.kt
@@ -1,5 +1,12 @@
 package com.example.fantasmo_android.screens.camera
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,6 +15,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.fantasmo_android.R
 import com.example.fantasmo_android.utils.DemoAppUtils.AppUtils.createStringDisplay
@@ -15,23 +24,39 @@ import com.google.ar.sceneform.ArSceneView
 import com.google.ar.sceneform.ux.ArFragment
 
 
-class CameraFragment: Fragment() {
+class CameraFragment: Fragment(), LocationListener {
 
     private lateinit var arSceneView: ArSceneView
     private lateinit var arFragment: ArFragment
     private lateinit var currentView: View
 
+    private lateinit var cameraTranslationTv: TextView
+    private lateinit var cameraAnglesTv: TextView
+    private lateinit var deviceCoorTv : TextView
+    private lateinit var checkParkingButton : Button
+
+    private lateinit var locationManager: LocationManager
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         currentView = inflater.inflate(R.layout.camera_fragment, container, false)
+
+        cameraTranslationTv = currentView.findViewById(R.id.cameraTranslation)
+        cameraAnglesTv = currentView.findViewById(R.id.cameraAnglesText)
+        checkParkingButton = currentView.findViewById(R.id.checkParkingButton)
+        deviceCoorTv = currentView.findViewById(R.id.coordinatesText)
+
+        locationManager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        getLocation()
+
         return currentView
     }
 
     /**
-     * To make any changes or get any values of the ArFragment always call onResume lifecycle
-     *
+     * To make any changes or get any values from ArFragment always call onResume lifecycle
      * */
     override fun onResume(){
         super.onResume()
@@ -48,42 +73,79 @@ class CameraFragment: Fragment() {
                     onUpdate()
                 }
             }
-            currentView.findViewById<Button>(R.id.checkParkingButton)?.setOnClickListener{
-                Log.d("CheckPark Pressed", "CheckPark")
+            checkParkingButton.setOnClickListener{
+                Log.d("CameraFragment-> CheckPark Pressed", "CheckPark")
             }
 
         }catch (e: Exception){
-            Log.d("ArFragment Null", "ArFragment")
+            Log.d("CameraFragment-> ArFragment Null", "ArFragment")
         }
     }
 
     /**
      * On any changes to the scene call onUpdate method to get arFrames and get the camera data
      * Data obtained from the sensor: Camera Translation and Camera Rotation values
-     *
      * */
     private fun onUpdate() {
         val arFrame = arSceneView.arFrame
 
         val cameraTranslation = arFrame?.androidSensorPose?.translation
-        currentView.findViewById<TextView>(R.id.cameraTranslation)?.text =
+        cameraTranslationTv.text =
             createStringDisplay("Camera Translation: ", cameraTranslation)
-        Log.d("Camera Translation", "CameraTranslation ${cameraTranslation.toString()}")
+        Log.d("CameraFragment-> Camera Translation", createStringDisplay("Camera Translation: ", cameraTranslation))
 
         val cameraRotation = arFrame?.androidSensorPose?.rotationQuaternion
-        currentView.findViewById<TextView>(R.id.cameraAnglesText)?.text =
-            createStringDisplay("Camera Rotation: ", cameraRotation)
-        Log.d("Camera Rotation", "CameraRotation ${cameraRotation.toString()}")
-
-    /*
-        val arCamera = arFrame?.camera
-        if (arCamera?.trackingState === TrackingState.TRACKING) {
-            val cameraPose: Pose = arCamera.displayOrientedPose
-            currentView.findViewById<TextView>(R.id.cameraTranslation)?.text = cameraPose.toString()
-            Log.d("Camera Translation", "CameraTranslation $cameraPose")
-        }*/
+        cameraAnglesTv.text =
+            createStringDisplay("Camera Angles: ", cameraRotation)
+        Log.d("CameraFragment-> Camera Angles", createStringDisplay("Camera Angles: ", cameraRotation))
     }
 
+    /**
+     * Gets system location through the app context
+     * Then checks if it has permission to ACCESS_FINE_LOCATION
+     * Keeps crashing at the first time when asking for location permissions
+     */
+    private fun getLocation() {
+        if (context?.let {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+            } != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            } else {
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            }
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+    }
 
+    @SuppressLint("SetTextI18n")
+    override fun onLocationChanged(location: Location) {
+        deviceCoorTv.text = "Device Latitude: ${location.latitude} , Longitude: ${location.longitude}"
+        Log.d("CameraFragment-> LocationChanged: ", "New Latitude: ${location.latitude} and New Longitude: ${location.longitude}")
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(requireContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        Log.d("CameraFragment-> OnRequestPermissionResult: ", "Granted")
+                    }
+                } else {
+                    // Permission is not granted
+                    Log.d("CameraFragment-> OnRequestPermissionResult: ", "Denied")
+                }
+                return
+            }
+        }
+    }
 }
--- FantasmoDemoApp/app/src/main/java/com/example/fantasmo_android/utils/DemoAppUtils.kt
+++ FantasmoDemoApp/app/src/main/java/com/example/fantasmo_android/utils/DemoAppUtils.kt
@@ -3,7 +3,7 @@ package com.example.fantasmo_android.utils
 class DemoAppUtils {
 
     object AppUtils{
-        fun createStringDisplay(s: String, cameraAttr: FloatArray?): CharSequence {
+        fun createStringDisplay(s: String, cameraAttr: FloatArray?): String {
             return s + String.format("%.2f", cameraAttr?.get(0)) + ", " +
                     String.format("%.2f", cameraAttr?.get(1)) + ", " + String.format("%.2f", cameraAttr?.get(2))
         }
