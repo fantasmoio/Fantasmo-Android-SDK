ac1597f6fed953226d874c7514b15f2e8547bd69
Tiago Morais <tiago.morais@fsstudio.com>
2021-03-16 18:17:26 +0000

    added SDK basic structure

---
+++ FantasmoSDK/app/src/main/java/com/fantasmo/sdk/FMConfiguration.kt
@@ -0,0 +1,38 @@
+package com.fantasmo.sdk
+
+import android.location.Location
+import com.fantasmo.sdk.fantasmosdk.BuildConfig
+
+/**
+ * Class to load configuration values like coordinates or server URLs.
+ */
+class FMConfiguration {
+
+    companion object {
+        /**
+         * API base url
+         */
+        fun getServerURL(): String {
+            return BuildConfig.FM_API_BASE_URL
+        }
+
+        /**
+         * Current location
+         */
+        fun getConfigLocation(): Location {
+            return if (BuildConfig.FLAVOR.startsWith("dev") &&
+                BuildConfig.FM_GPS_LAT_LONG.isNotBlank()
+            ) {
+                val locationComponents = BuildConfig.FM_GPS_LAT_LONG.split(",").toTypedArray()
+
+                val location = Location("")
+                location.latitude = locationComponents[0].toDouble()
+                location.longitude = locationComponents[1].toDouble()
+
+                location
+            } else {
+                Location("")
+            }
+        }
+    }
+}
