066a558e66e1c4fea18126499f7d602d45ed4a5f
José Silva <jose.silva@fsstudio.com>
2021-03-17 12:08:30 +0000

    Added Switch button listeners

--- FantasmoDemoApp/app/src/main/java/com/example/fantasmo_android/screens/camera/CameraFragment.kt
+++ FantasmoDemoApp/app/src/main/java/com/example/fantasmo_android/screens/camera/CameraFragment.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
@@ -17,8 +16,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Switch
 import android.widget.TextView
-import androidx.core.app.ActivityCompat
+import android.widget.ToggleButton
 import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.fragment.app.Fragment
@@ -39,11 +39,16 @@ class CameraFragment: Fragment(), LocationListener {
     private lateinit var deviceCoorTv : TextView
     private lateinit var checkParkingButton : Button
 
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private lateinit var localizeToggleButton : Switch
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private lateinit var anchorToggleButton : Switch
+
     private lateinit var locationManager: LocationManager
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
 
         currentView = inflater.inflate(R.layout.camera_fragment, container, false)
@@ -52,6 +57,8 @@ class CameraFragment: Fragment(), LocationListener {
         cameraAnglesTv = currentView.findViewById(R.id.cameraAnglesText)
         checkParkingButton = currentView.findViewById(R.id.checkParkingButton)
         deviceCoorTv = currentView.findViewById(R.id.coordinatesText)
+        localizeToggleButton = currentView.findViewById(R.id.localizeToggle)
+        anchorToggleButton = currentView.findViewById(R.id.anchorToggle)
 
         locationManager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
@@ -80,8 +87,29 @@ class CameraFragment: Fragment(), LocationListener {
                     onUpdate()
                 }
             }
+
             checkParkingButton.setOnClickListener{
                 Log.d("CameraFragment-> CheckPark Pressed", "CheckPark")
+            }
+
+            localizeToggleButton.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    // if toggle button is enabled/on
+                    Log.d("CameraFragment-> LocalizeToggle ", "Enabled")
+                } else {
+                    // If toggle button is disabled/off
+                    Log.d("CameraFragment-> LocalizeToggle ", "Disabled")
+                }
+            }
+
+            anchorToggleButton.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    // if toggle button is enabled/on
+                    Log.d("CameraFragment-> AnchorToggle ", "Enabled")
+                } else {
+                    // If toggle button is disabled/off
+                    Log.d("CameraFragment-> AnchorToggle ", "Disabled")
+                }
             }
 
         }catch (e: Exception){
@@ -101,20 +129,20 @@ class CameraFragment: Fragment(), LocationListener {
         cameraTranslationTv.text =
             createStringDisplay("Camera Translation: ", cameraTranslation)
         Log.d(
-            "CameraFragment-> Camera Translation", createStringDisplay(
+                "CameraFragment-> Camera Translation", createStringDisplay(
                 "Camera Translation: ",
                 cameraTranslation
-            )
+        )
         )
 
         val cameraRotation = arFrame?.androidSensorPose?.rotationQuaternion
         cameraAnglesTv.text =
             createStringDisplay("Camera Angles: ", cameraRotation)
         Log.d(
-            "CameraFragment-> Camera Angles", createStringDisplay(
+                "CameraFragment-> Camera Angles", createStringDisplay(
                 "Camera Angles: ",
                 cameraRotation
-            )
+        )
         )
     }
 
@@ -125,8 +153,8 @@ class CameraFragment: Fragment(), LocationListener {
     private fun getLocation() {
         if ((context?.let {
                 checkSelfPermission(
-                    it,
-                    Manifest.permission.ACCESS_FINE_LOCATION
+                        it,
+                        Manifest.permission.ACCESS_FINE_LOCATION
                 )
             } != PackageManager.PERMISSION_GRANTED) &&
                 (context?.let{
@@ -136,11 +164,11 @@ class CameraFragment: Fragment(), LocationListener {
                     )
                 } != PackageManager.PERMISSION_GRANTED)) {
             if (shouldShowRequestPermissionRationale(
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                )) {
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    )) {
                 this.requestPermissions(
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.CAMERA),
-                    1
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.CAMERA),
+                        1
                 )
             } else {
                 this.requestPermissions(
@@ -157,36 +185,36 @@ class CameraFragment: Fragment(), LocationListener {
     override fun onLocationChanged(location: Location) {
         deviceCoorTv.text = "Device Lat: ${location.latitude} , Long: ${location.longitude}"
         Log.d(
-            "CameraFragment-> LocationChanged: ",
-            "New Latitude: ${location.latitude} and New Longitude: ${location.longitude}"
+                "CameraFragment-> LocationChanged: ",
+                "New Latitude: ${location.latitude} and New Longitude: ${location.longitude}"
         )
     }
 
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
     ) {
         when (requestCode) {
             1 -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     if (ContextCompat.checkSelfPermission(
-                            requireContext(),
-                            Manifest.permission.ACCESS_FINE_LOCATION
-                        ) == PackageManager.PERMISSION_GRANTED
+                                    requireContext(),
+                                    Manifest.permission.ACCESS_FINE_LOCATION
+                            ) == PackageManager.PERMISSION_GRANTED
                     ) {
                         Log.d("CameraFragment-> OnRequestPermissionResult: Location", "Granted")
                     }
-                    if((ContextCompat.checkSelfPermission(
+                    if ((ContextCompat.checkSelfPermission(
                                     requireContext(),
                                     Manifest.permission.CAMERA
                             ) == PackageManager.PERMISSION_GRANTED
-                                    )){
+                                    )) {
                         Log.d("CameraFragment-> OnRequestPermissionResult: Camera", "Granted")
                     }
                 } else {
                     // Permission is not granted
-                        activity?.finish()
+                    activity?.finish()
                     Log.d("CameraFragment-> OnRequestPermissionResult: ", "Denied")
                 }
                 return
