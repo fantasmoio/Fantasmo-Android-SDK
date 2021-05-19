db4c3bd5c2b95ea887a22bed218f17c2406a1ced
Tiago Morais <tiago.morais@fsstudio.com>
2021-05-19 15:07:53 +0100

    Fixed ZoneInRadius Bug

--- FantasmoSDK/app/src/main/java/com/fantasmo/sdk/network/FMNetworkManager.kt
+++ FantasmoSDK/app/src/main/java/com/fantasmo/sdk/network/FMNetworkManager.kt
@@ -114,9 +114,9 @@ class FMNetworkManager(
                         Gson().fromJson(resultResponse, ZoneInRadiusResponse::class.java)
                     onCompletion(inRadius.result.toBoolean())
                 } catch (e: JSONException) {
+                    onCompletion(false)
                     e.printStackTrace()
                 }
-                onCompletion(true)
             },
             Response.ErrorListener { error ->
                 processAndLogError(error)
