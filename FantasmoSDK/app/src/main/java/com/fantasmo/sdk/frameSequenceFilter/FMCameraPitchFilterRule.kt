415f028ab88d761543b7d3c95cbe81bfd1c9c68f
José Silva <jose.silva@fsstudio.com>
2021-06-25 17:03:48 +0100

    Fix CameraPitch Rule

--- FantasmoSDK/app/src/main/java/com/fantasmo/sdk/frameSequenceFilter/FMCameraPitchFilterRule.kt
+++ FantasmoSDK/app/src/main/java/com/fantasmo/sdk/frameSequenceFilter/FMCameraPitchFilterRule.kt
@@ -19,13 +19,13 @@ class FMCameraPitchFilterRule : FMFrameSequenceFilterRule {
      */
     override fun check(arFrame: Frame): Pair<FMFrameFilterResult, FMFrameFilterFailure> {
         // Angle of X-plane of device sensor system (Y-Axis up).
-        val x = arFrame.androidSensorPose.rotationQuaternion[0]
+        val x = arFrame.camera.displayOrientedPose.rotationQuaternion[0]
 
         return when {
             abs(x) <= radianThreshold -> {
                 Pair(FMFrameFilterResult.ACCEPTED, FMFrameFilterFailure.ACCEPTED)
             }
-            x > 0 -> {
+            x < 0 -> {
                 Pair(FMFrameFilterResult.REJECTED, FMFrameFilterFailure.PITCHTOOHIGH)
             }
             else -> {
