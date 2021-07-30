bcc4d42c6fd308f73dcf3bb14bfe728db855261e
José Silva <jose.silva@fsstudio.com>
2021-07-30 16:55:31 +0100

    Added total of filtered frames counter

--- FantasmoSDK/app/src/main/java/com/fantasmo/sdk/models/analytics/FrameFilterRejectionStatisticsAccumulator.kt
+++ FantasmoSDK/app/src/main/java/com/fantasmo/sdk/models/analytics/FrameFilterRejectionStatisticsAccumulator.kt
@@ -7,12 +7,13 @@ class FrameFilterRejectionStatisticsAccumulator {
 
     private val TAG = "FrameFilterStatistics"
 
+    private var totalFrameCount = 0
     private var excessiveTiltFrameCount = 0
     private var excessiveBlurFrameCount = 0
     private var insufficientMotionFrameCount = 0
 
-
     fun accumulate(result: FMFrameFilterFailure) {
+        totalFrameCount += 1
         when (result) {
             FMFrameFilterFailure.MOVINGTOOFAST -> {
                 excessiveBlurFrameCount += 1
@@ -27,12 +28,14 @@ class FrameFilterRejectionStatisticsAccumulator {
                 excessiveTiltFrameCount += 1
             }
         }
+        Log.d(TAG,"Tilt: $excessiveTiltFrameCount, " +
+                "Blur: $excessiveBlurFrameCount, " +
+                "Motion: $insufficientMotionFrameCount," +
+                "Total: $totalFrameCount")
     }
 
     fun reset() {
-        Log.d(TAG,"Tilt: $excessiveTiltFrameCount, " +
-                "Blur: $excessiveBlurFrameCount, " +
-                "Motion: $insufficientMotionFrameCount")
+        totalFrameCount = 0
         excessiveTiltFrameCount = 0
         excessiveBlurFrameCount = 0
         insufficientMotionFrameCount = 0
