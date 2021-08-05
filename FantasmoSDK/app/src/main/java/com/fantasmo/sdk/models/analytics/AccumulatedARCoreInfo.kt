6a3f2cfc886d8c29d75fb614c3682ad2fd1eb453
José Silva <jose.silva@fsstudio.com>
2021-08-05 15:08:22 +0100

    Fixed unit testing and added more documentation

--- FantasmoSDK/app/src/main/java/com/fantasmo/sdk/models/analytics/AccumulatedARCoreInfo.kt
+++ FantasmoSDK/app/src/main/java/com/fantasmo/sdk/models/analytics/AccumulatedARCoreInfo.kt
@@ -2,14 +2,27 @@ package com.fantasmo.sdk.models.analytics
 
 import com.google.ar.core.Frame
 
+/**
+ * Class responsible for accumulating information and statistics about how the ARCore is behaving
+ * on the SDK. Also collects information about the filtered frames.
+ */
 class AccumulatedARCoreInfo {
 
     private var trackingStateFrameStatistics = TrackingStateFrameStatistics()
 
+    /**
+     * When invoked, this calls all the reset method on the class required to collect all the info
+     * This should be called on each time a startUpdatingLocation is called
+     */
     fun reset(){
         trackingStateFrameStatistics.reset()
     }
 
+    /**
+     * When invoked, this method gives a copy of the frame (collected during the localize request
+     * call) to each class involved in the statistics collection
+     * @param arFrame: frame collected during localize request
+     */
     fun update(arFrame: Frame) {
         trackingStateFrameStatistics.update(arFrame)
     }
