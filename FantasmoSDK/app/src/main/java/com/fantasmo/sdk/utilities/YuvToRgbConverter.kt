41d0ac58075feb6d018fe2cc76a37e0a34893c9a
Sébastien Roger <basti.roger@gmail.com>
2022-01-27 18:16:37 +0100

    Fixes YUVToRGBConverter to feed the right images to FMImageQualityFilter

--- FantasmoSDK/app/src/main/java/com/fantasmo/sdk/utilities/YuvToRgbConverter.kt
+++ FantasmoSDK/app/src/main/java/com/fantasmo/sdk/utilities/YuvToRgbConverter.kt
@@ -1,16 +1,13 @@
 package com.fantasmo.sdk.utilities
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.ImageFormat
-import android.graphics.Rect
+import android.graphics.*
 import android.media.Image
 import android.os.Build
 import android.renderscript.*
 import android.util.Log
 import androidx.annotation.RequiresApi
 import com.fantasmo.sdk.FMUtility
-import com.fantasmo.sdk.fantasmosdk.BuildConfig
 import com.google.ar.core.Frame
 import com.google.ar.core.exceptions.DeadlineExceededException
 import com.google.ar.core.exceptions.NotYetAvailableException
@@ -19,7 +16,10 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import java.nio.ByteBuffer
+import android.renderscript.Allocation
+
+import android.renderscript.RenderScript
+
 
 @RequiresApi(Build.VERSION_CODES.KITKAT)
 class YuvToRgbConverter(
@@ -31,8 +31,6 @@ class YuvToRgbConverter(
     private val rs = RenderScript.create(context)
     private val yuvToRgbIntrinsic = ScriptIntrinsicYuvToRGB.create(rs, Element.U8_4(rs))
 
-    private var pixelCount: Int = -1
-    private lateinit var yuvBuffer: ByteBuffer
     private lateinit var inputAllocation: Allocation
     private lateinit var outputAllocation: Allocation
 
@@ -48,25 +46,35 @@ class YuvToRgbConverter(
      */
     @Synchronized
     fun toBitmap(frame: Frame): Bitmap? {
-        val height = frame.camera.imageIntrinsics.imageDimensions[0]
-        val width = frame.camera.imageIntrinsics.imageDimensions[1]
+        val width = frame.camera.imageIntrinsics.imageDimensions[0]
+        val height = frame.camera.imageIntrinsics.imageDimensions[1]
         val output = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val image = acquireFrameImage(frame) ?: return null
 
-        // Ensure that the intermediate output byte buffer is allocated
-        if (!::yuvBuffer.isInitialized) {
-            pixelCount = image.cropRect.width() * image.cropRect.height()
-            // Bits per pixel is an average for the whole image, so it's useful to compute the size
-            // of the full buffer but should not be used to determine pixel offsets
-            val pixelSizeBits = ImageFormat.getBitsPerPixel(ImageFormat.YUV_420_888)
-            yuvBuffer = ByteBuffer.allocateDirect(pixelCount * pixelSizeBits / 8)
-        }
+        val cameraPlaneY = image.planes[0].buffer
+        val cameraPlaneV = image.planes[1].buffer
+        val cameraPlaneU = image.planes[2].buffer
 
-        // Rewind the buffer; no need to clear it since it will be filled
-        yuvBuffer.rewind()
+        //Use the buffers to create a new byteArray that
+        val compositeByteArray =
+            ByteArray(cameraPlaneY.capacity() + cameraPlaneU.capacity() + cameraPlaneV.capacity())
 
-        // Get the YUV data in byte array form using NV21 format
-        imageToByteBuffer(image, yuvBuffer.array())
+        cameraPlaneY.get(compositeByteArray, 0, cameraPlaneY.capacity())
+        cameraPlaneU.get(compositeByteArray, cameraPlaneY.capacity(), cameraPlaneU.capacity())
+        cameraPlaneV.get(
+            compositeByteArray,
+            cameraPlaneY.capacity() + cameraPlaneU.capacity(),
+            cameraPlaneV.capacity()
+        )
+
+        val yuvImage = YuvImage(
+            compositeByteArray,
+            ImageFormat.NV21,
+            image.width,
+            image.height,
+            null
+        )
+
         GlobalScope.launch(defaultDispatcher) {
             FMUtility.setFrameQualityTest(image)
             image.close()
@@ -75,18 +83,19 @@ class YuvToRgbConverter(
         if (!::inputAllocation.isInitialized) {
             // Explicitly create an element with type NV21, since that's the pixel format we use
             val elemType = Type.Builder(rs, Element.YUV(rs)).setYuvFormat(ImageFormat.NV21).create()
-            inputAllocation = Allocation.createSized(rs, elemType.element, yuvBuffer.array().size)
+            inputAllocation = Allocation.createSized(rs, elemType.element, yuvImage.yuvData.size)
         }
         if (!::outputAllocation.isInitialized) {
             outputAllocation = Allocation.createFromBitmap(rs, output)
         }
 
         // Convert NV21 format YUV to RGB
-        inputAllocation.copyFrom(yuvBuffer.array())
+        inputAllocation.copyFrom(yuvImage.yuvData)
         yuvToRgbIntrinsic.setInput(inputAllocation)
         yuvToRgbIntrinsic.forEach(outputAllocation)
         outputAllocation.copyTo(output)
-        return Bitmap.createScaledBitmap(output, imageWidth, imageHeight, true)
+
+        return output
     }
 
     /**
@@ -105,130 +114,5 @@ class YuvToRgbConverter(
             Log.e(TAG, "ResourceExhaustedException")
         }
         return null
-    }
-
-    /**
-     * Creates ByteBuffer from the Image acquired from the
-     * ARFrame and resizes it to the specified model shape.
-     * @param image Image acquired from ARFrame
-     * @param outputBuffer Buffer to be filled with the image
-     */
-    private fun imageToByteBuffer(image: Image, outputBuffer: ByteArray) {
-
-        if (BuildConfig.DEBUG && image.format != ImageFormat.YUV_420_888) {
-            error("Assertion failed")
-        }
-
-        val imageCrop = image.cropRect
-        val imagePlanes = image.planes
-
-        imagePlanes.forEachIndexed { planeIndex, plane ->
-            // How many values are read in input for each output value written
-            // Only the Y plane has a value for every pixel, U and V have half the resolution i.e.
-            //
-            // Y Plane            U Plane    V Plane
-            // ===============    =======    =======
-            // Y Y Y Y Y Y Y Y    U U U U    V V V V
-            // Y Y Y Y Y Y Y Y    U U U U    V V V V
-            // Y Y Y Y Y Y Y Y    U U U U    V V V V
-            // Y Y Y Y Y Y Y Y    U U U U    V V V V
-            // Y Y Y Y Y Y Y Y
-            // Y Y Y Y Y Y Y Y
-            // Y Y Y Y Y Y Y Y
-            val outputStride: Int
-
-            // The index in the output buffer the next value will be written at
-            // For Y it's zero, for U and V we start at the end of Y and interleave them i.e.
-            //
-            // First chunk        Second chunk
-            // ===============    ===============
-            // Y Y Y Y Y Y Y Y    V U V U V U V U
-            // Y Y Y Y Y Y Y Y    V U V U V U V U
-            // Y Y Y Y Y Y Y Y    V U V U V U V U
-            // Y Y Y Y Y Y Y Y    V U V U V U V U
-            // Y Y Y Y Y Y Y Y
-            // Y Y Y Y Y Y Y Y
-            // Y Y Y Y Y Y Y Y
-            var outputOffset: Int
-
-            when (planeIndex) {
-                0 -> {
-                    outputStride = 1
-                    outputOffset = 0
-                }
-                1 -> {
-                    outputStride = 2
-                    // For NV21 format, U is in odd-numbered indices
-                    outputOffset = pixelCount + 1
-                }
-                2 -> {
-                    outputStride = 2
-                    // For NV21 format, V is in even-numbered indices
-                    outputOffset = pixelCount
-                }
-                else -> {
-                    // Image contains more than 3 planes, something strange is going on
-                    return@forEachIndexed
-                }
-            }
-
-            val planeBuffer = plane.buffer
-            val rowStride = plane.rowStride
-            val pixelStride = plane.pixelStride
-
-            // We have to divide the width and height by two if it's not the Y plane
-            val planeCrop = if (planeIndex == 0) {
-                imageCrop
-            } else {
-                Rect(
-                    imageCrop.left / 2,
-                    imageCrop.top / 2,
-                    imageCrop.right / 2,
-                    imageCrop.bottom / 2
-                )
-            }
-
-            val planeWidth = planeCrop.width()
-            val planeHeight = planeCrop.height()
-
-            // Intermediate buffer used to store the bytes of each row
-            val rowBuffer = ByteArray(plane.rowStride)
-
-            // Size of each row in bytes
-            val rowLength = if (pixelStride == 1 && outputStride == 1) {
-                planeWidth
-            } else {
-                // Take into account that the stride may include data from pixels other than this
-                // particular plane and row, and that could be between pixels and not after every
-                // pixel:
-                //
-                // |---- Pixel stride ----|                    Row ends here --> |
-                // | Pixel 1 | Other Data | Pixel 2 | Other Data | ... | Pixel N |
-                //
-                // We need to get (N-1) * (pixel stride bytes) per row + 1 byte for the last pixel
-                (planeWidth - 1) * pixelStride + 1
-            }
-
-            for (row in 0 until planeHeight) {
-                // Move buffer position to the beginning of this row
-                planeBuffer.position(
-                    (row + planeCrop.top) * rowStride + planeCrop.left * pixelStride
-                )
-
-                if (pixelStride == 1 && outputStride == 1) {
-                    // When there is a single stride value for pixel and output, we can just copy
-                    // the entire row in a single step
-                    planeBuffer.get(outputBuffer, outputOffset, rowLength)
-                    outputOffset += rowLength
-                } else {
-                    // When either pixel or output have a stride > 1 we must copy pixel by pixel
-                    planeBuffer.get(rowBuffer, 0, rowLength)
-                    for (col in 0 until planeWidth) {
-                        outputBuffer[outputOffset] = rowBuffer[col * pixelStride]
-                        outputOffset += outputStride
-                    }
-                }
-            }
-        }
     }
 }
