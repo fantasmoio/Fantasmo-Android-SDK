47770f784e1f8dc808032b0eeaf9ceb6397b2327
José Silva <80684851+jpvsilva-fss@users.noreply.github.com>
2022-02-03 12:23:24 +0000

    Feature/mw 512 feature allow skipping the qr code (#90)
    * Added enableQRCodeScanner control variable
    
    * Added qrCodeScan Switch enabler
    
    * Fixed default behavior to always true
    
    * Added anchor on skip QR scan code
    
    * Updated customizable fragment example of DemoApp

--- FantasmoSDK/app/src/main/java/com/fantasmo/sdk/views/FMParkingView.kt
+++ FantasmoSDK/app/src/main/java/com/fantasmo/sdk/views/FMParkingView.kt
@@ -54,6 +54,12 @@ class FMParkingView @JvmOverloads constructor(
     var isSimulation = false
 
     /**
+     * Controls QR code scanner. When set to `true` enables a QR code scanning session.
+     * Default behavior is to be set on `true`
+     */
+    var enableQRCodeScanner = true
+
+    /**
      * Controls whether this class uses its own internal LocationManager to automatically receive location updates. Default is `true`.
      * When set to `false` it is expected that location updates will be manually provided via the `updateLocation()` method.
      */
@@ -200,7 +206,15 @@ class FMParkingView @JvmOverloads constructor(
             statistics.visibility = View.GONE
         }
 
-        startQRScanning()
+        if (enableQRCodeScanner){
+            startQRScanning()
+        }
+        else {
+            // Set an AR anchor now
+            fmARCoreView.startAnchor()
+            state = State.QRSCANNING
+            startLocalizing()
+        }
     }
 
     /**
