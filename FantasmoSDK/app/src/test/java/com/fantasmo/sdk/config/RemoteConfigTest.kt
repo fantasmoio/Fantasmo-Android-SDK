5e84d8f4b08ab6a89d126993654c8cd0c95d7f9a
Sébastien Roger <basti.roger@gmail.com>
2022-02-10 15:32:39 +0100

    Changed test names, tested all fields in RemoteConfig class and loaded default config in tests

--- FantasmoSDK/app/src/test/java/com/fantasmo/sdk/config/RemoteConfigTest.kt
+++ FantasmoSDK/app/src/test/java/com/fantasmo/sdk/config/RemoteConfigTest.kt
@@ -1,10 +1,12 @@
 package com.fantasmo.sdk.config
 
 import androidx.test.platform.app.InstrumentationRegistry
+import com.google.gson.Gson
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import java.io.IOException
 
 @RunWith(RobolectricTestRunner::class)
 class RemoteConfigTest {
@@ -49,17 +51,45 @@ class RemoteConfigTest {
             imageQualityFilterModelUri = null,
             imageQualityFilterModelVersion = "0.1.0"
         )
+
+        var defaultConfig : RemoteConfig.Config
+        init {
+            val filePath = "config/default-config.json"
+            val jsonString: String = try {
+                val inputStream = InstrumentationRegistry.getInstrumentation().context.assets.open(filePath)
+                val size = inputStream.available()
+                val buffer = ByteArray(size)
+                inputStream.read(buffer)
+                inputStream.close()
+                String(buffer, Charsets.UTF_8)
+            } catch (e: IOException) {
+                ""
+            }
+            defaultConfig = Gson().fromJson(jsonString, RemoteConfig.Config::class.java)
+        }
     }
+
     @Test
-    fun testRemoteConfigNullId() {
+    fun testUseStoredConfigWhenRemoteConfigIdIsNull() {
         RemoteConfig.updateConfig(InstrumentationRegistry.getInstrumentation().context, "null")
-        assertEquals(RemoteConfig.remoteConfig.remoteConfigId, remoteConfig.remoteConfigId)
+
+        // Fancy Java reflection stuff to get all fields in the class from kotlin
+        val fields = RemoteConfig.Config::class.java.declaredFields
+        for(field in fields) {
+            field.isAccessible = true
+            assertEquals(field.get(RemoteConfig.remoteConfig), field.get(defaultConfig))
+        }
     }
 
     @Test
-    fun testRemoteConfigEmptyArray() {
+    fun testUseStoredConfigWhenRemoteConfigIdIsArray() {
         RemoteConfig.updateConfig(InstrumentationRegistry.getInstrumentation().context, "[]")
-        assertEquals(RemoteConfig.remoteConfig.remoteConfigId, remoteConfig.remoteConfigId)
+        
+        // Fancy Java reflection stuff to get all fields in the class from kotlin
+        val fields = RemoteConfig.Config::class.java.declaredFields
+        for(field in fields) {
+            field.isAccessible = true
+            assertEquals(field.get(RemoteConfig.remoteConfig), field.get(defaultConfig))
+        }
     }
-
 }
