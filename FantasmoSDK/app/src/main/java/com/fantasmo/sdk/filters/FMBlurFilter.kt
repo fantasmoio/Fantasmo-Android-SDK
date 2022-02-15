c5debd4dbca4035e2100576323bd1273aaea55e9
Sébastien Roger <basti.roger@gmail.com>
2022-02-15 11:45:18 +0100

    Standard deviation computation fixed in FMBlurFilter

--- FantasmoSDK/app/src/main/java/com/fantasmo/sdk/filters/FMBlurFilter.kt
+++ FantasmoSDK/app/src/main/java/com/fantasmo/sdk/filters/FMBlurFilter.kt
@@ -138,7 +138,7 @@ class FMBlurFilter(
             var avg = 0.0
             histogramBins.forEachIndexed { index, bin -> avg += index * bin / (256.0 * convolveOutputAllocation.bytesSize) }
             var stdDev = 0.0
-            histogramBins.forEachIndexed { index, bin -> stdDev += (index * bin / 256.0 - avg).pow(2.0) / convolveOutputAllocation.bytesSize}
+            histogramBins.forEachIndexed { index, bin -> stdDev += (index / 256.0 - avg).pow(2.0) * bin / convolveOutputAllocation.bytesSize}
 
             return (sqrt(stdDev) * 100.0).toFloat()
         }
