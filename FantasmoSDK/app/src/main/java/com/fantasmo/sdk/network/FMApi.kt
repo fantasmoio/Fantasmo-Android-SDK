3b1f76599df59d44c26def8be3529e2c8dd032df
Sébastien Roger <basti.roger@gmail.com>
2022-03-23 15:19:20 +0100

    Made capturedAt and location.timestamp floats, Closes MW-625 (#107)
    * location.timestamp and capturedAt are now floats in seconds, Closes MW-625
    
    * Not formatting string in a specific way in capturedAt param, both are now consistent
    
    * Corrected string in FMNetworkManagerTest as well

--- FantasmoSDK/app/src/main/java/com/fantasmo/sdk/network/FMApi.kt
+++ FantasmoSDK/app/src/main/java/com/fantasmo/sdk/network/FMApi.kt
@@ -246,7 +246,7 @@ class FMApi(
 
         val params = hashMapOf<String, String>()
         val gson = Gson()
-        params["capturedAt"] = System.currentTimeMillis().toString()
+        params["capturedAt"] = (System.currentTimeMillis().toDouble() / 1000.0).toString()
         params["gravity"] = gson.toJson(pose.orientation)
         params["uuid"] = UUID.randomUUID().toString()
         params["location"] = gson.toJson(location)
