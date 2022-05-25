542ba4bcebe14c44c4f0047624ddfe28766c777d
Sébastien Roger <sebastien@fantasmo.io>
2022-05-25 10:47:38 +0200

    Incrementing totalFramesUploaded before request, not on response, Fixes LOC-55 (#120)

--- FantasmoSDK/app/src/main/java/com/fantasmo/sdk/FMLocationManager.kt
+++ FantasmoSDK/app/src/main/java/com/fantasmo/sdk/FMLocationManager.kt
@@ -239,7 +239,8 @@ class FMLocationManager(private val context: Context) : FMFrameEvaluatorChainLis
         val localizeRequest = createLocalizationRequest(fmFrame)
         fmLocationListener?.didBeginUpload(fmFrame)
         activeUploads.add(fmFrame)
-
+        totalFramesUploaded++
+        
         coroutineScope.launch {
             fmApi?.sendLocalizeRequest(
                 fmFrame,
@@ -251,14 +252,12 @@ class FMLocationManager(private val context: Context) : FMFrameEvaluatorChainLis
                     fmLocationListener?.didUpdateLocation(
                         result
                     )
-                    totalFramesUploaded++
                     updateStateAfterLocalization()
                 },
                 { error ->
                     Log.e(TAG, "localize: $error")
                     activeUploads.removeAll { it == fmFrame }
                     fmLocationListener?.didFailWithError(error, null)
-                    totalFramesUploaded++
                     errors.add(error)
                     updateStateAfterLocalization()
                 })
